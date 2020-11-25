2f86be87db0fc242c81ecefdf756e8e634ac380c
Misha <chiriba.misha@gmail.com>
2020-11-25 17:08:11 +0200

    25-11-2020-JS

---
+++ 25-11-2020_JS/js/main.js
@@ -0,0 +1,35 @@
+let result = ' ';
+for (let i = 2; i <= 14; i++) {
+  if (i % 2 == 0) {
+    result += i + ' ';
+  }
+}
+console.log(result);
+
+
+let result1 = ' ';
+for (let a = 33; a <= 45.2; a++) {
+  if (a % 2) {
+    result1 += a + ' ';
+  }
+}
+console.log(result1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
